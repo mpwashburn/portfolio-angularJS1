2ed3216446913b6562229650a2b0020dcb0d6882
Michael Washburn <washburn.michael@gmail.com>
2016-05-27 15:28:08 -0700

    about and prjects now rendering

--- public/app/portfolio.module.js
+++ public/app/portfolio.module.js
@@ -2,20 +2,24 @@
 (function(){
   'use strict';
 
-angular.module('portfolio', [
-  'ui.router',
-  'portfolio.projects'
-  ])
-  .config(function ($stateProvider, $urlRouterProvider) {
-          $stateProvider
-              .state('portfolio', {
-                  url: '',
-                  abstract: true
-              });
-              $urlRouterProvider.otherwise('/');
-    })
+angular.module('portfolio', [])
   .controller('PortfolioCtrl', function ($scope) {
-    console.log("PortfolioCtrl running");
+    $scope.projects = [
+    {
+    id: 0,
+    imageUrl:'img/Everest_Game_SS.jpg',
+    title: 'Everest Trivia',
+    languages: 'HTML | CSS | Javascript',
+    description: 'Simple trivia project completed in week 4'
+  },
+  {
+    id: 1,
+    imageUrl:'img/TTT.jpg',
+    title: 'Tic Tac Toe',
+    languages: 'HTML | CSS | Javascript',
+    description: 'Tic Tac Toe project completed in week 1'
+  }
+    ];
   });
 
 
